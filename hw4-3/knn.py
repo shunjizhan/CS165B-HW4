ee663fa7b455b98ba3be15fc3312069868752ea1
shunji zhan <shunji.zhan@procore.com>
2017-05-26 11:14:24 -0700

    added point class

--- hw4-3/knn.py
+++ hw4-3/knn.py
@@ -9,13 +9,29 @@ def readFile(filename):
   lines = [toF(line.rstrip('\n').rstrip('\r').split()) for line in open(filename)]
   return lines
 
+class Point:
+  def __init__(self, description):
+    description = [float(x) for x in description]
+    self.coordinate = description[:-1]
+    self.label = description[-1]
+
+  def show(self):
+    for i in range(len(self.coordinate)):
+      print self.coordinate[i],
+    print " -- ", self.label
+
 ##### training
 trainingData = readFile(sys.argv[1])
 M = int(trainingData[0][0])
 N = int(trainingData[0][1])
 trainingData = trainingData[1:]
 
-print trainingData
+Points = []
+for i in range(M):
+  Points.append(Point(trainingData[i]))
+
+for j in range(M):
+  Points[j].show()
 
 ##### testing
 testingData = readFile(sys.argv[2])
