e11d7c542a75e4a487c2c72e9a4a0ce010bb0130
shunji zhan <shunji.zhan@procore.com>
2017-05-26 07:35:21 -0700

    finished p1

--- hw4-1/mahadist.py
+++ hw4-1/mahadist.py
@@ -1,5 +1,6 @@
 import sys
 from numpy import matrix
+from math import sqrt
 
 def toF(array):
   return [float(x) for x in array]
@@ -25,26 +26,56 @@ def findCentroids(data, dimension):
 
   return matrix(centroid)
 
+def printCentroid(centroid):
+  print "Centroid:"
+  n = len(centroid)
+  for i in range(n):
+    print centroid[i], " ",
+  print " "
 
+def printCov(cov):
+  print "Covariance Matrix:"
+  n = len(cov)
+  m = len(cov[0])
+  for i in range(n):
+    for j in range(m):
+      print cov[i][j], " ",
+    print " "
 
+def printPoint(p):
+  print p[0], " ", p[1], " -- ",
 
-
-
-
+##### training
 trainingData = readFile(sys.argv[1])
-testingData = readFile(sys.argv[2])
-
 M = int(trainingData[0][0])
 N = int(trainingData[0][1])
 trainingData = trainingData[1:]
 
-# print type(N)
-
 centroid =  findCentroids(trainingData, N)
 trainingData = matrix(trainingData)
-Cov = (trainingData.T * trainingData) / M - centroid.T * centroid
+XZ = trainingData - centroid
+covariance = XZ.T * XZ / M
+# covariance = (trainingData.T * trainingData) / M - centroid.T * centroid
 
-print centroid
+printCentroid(centroid.tolist()[0])
+printCov(covariance.tolist())
+
+##### testing
+testingData = readFile(sys.argv[2])
+M_test = int(testingData[0][0])
+N_test = int(testingData[0][1])
+testingData = testingData[1:]
+
+print "Distances:"
+for i in range(len(testingData)):
+  p = matrix(testingData[i])
+  distance = sqrt( (p - centroid) * covariance.I * (p - centroid).T )
+  print str(i) + ". ",
+  printPoint(testingData[i])
+  print distance
+
+
+
 
 
 
